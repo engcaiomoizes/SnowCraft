d172c06ff76ab9aa3c1cc2944b5ddaf79121c824
Caio Moizes Santos <engcaiomoizes@gmail.com>
2025-07-14 08:36:35 -0300

    first commit

---
+++ src/main/java/br/com/caiomoizes/snowCraft/SnowCraft.java
@@ -0,0 +1,17 @@
+package br.com.caiomoizes.snowCraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SnowCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
